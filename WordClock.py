999b8a3a5a2982ac387a2690377ce41187ea2cc2
Thomas Lötzer <thomas@loetzer.com>
2015-08-14 19:49:00 +0200

    Finished stringifying minutes

--- WordClock.py
+++ WordClock.py
@@ -2,10 +2,13 @@ class WordClock:
     def stringify(self, time):
         stringified = "it is "
 
-        if time.minute in [59]:
-            stringified += str(time.hour + 1) + " " + "o'clock"
-        if time.minute in [0]:
-            stringified += str(time.hour) + " " + "o'clock"
+        roundedMin = int((round(time.minute / 5.0)) * 5)
+
+        if roundedMin == 60:
+            stringified += str(time.hour + 1) + " o'clock"
+        elif roundedMin == 0:
+            stringified += str(time.hour) + " o'clock"
         else:
-            pass
+            stringified += str(time.hour) + " " + str(roundedMin)
+
         return stringified
--- WordClockTest.py
+++ WordClockTest.py
@@ -13,5 +13,31 @@ class WordClockTest(unittest.TestCase):
     def testOneMinuteToFullHour(self):
         self.assertEquals(self.wordclock.stringify(datetime.time(11, 59, 0)), "it is 12 o'clock")
 
+    def testTwoMinutesToFullHour(self):
+        self.assertEquals(self.wordclock.stringify(datetime.time(11, 58, 0)), "it is 12 o'clock")
+
+    def testOneMinuteAfterFullHour(self):
+        self.assertEquals(self.wordclock.stringify(datetime.time(12, 01, 0)), "it is 12 o'clock")
+
+    def testTwoMinutesAfterFullHour(self):
+        self.assertEquals(self.wordclock.stringify(datetime.time(12, 02, 0)), "it is 12 o'clock")
+
+    def testTwoMinutesToTwenty(self):
+        self.assertEquals(self.wordclock.stringify(datetime.time(12, 18, 0)), "it is 12 20")
+
+    def testOneMinuteToTwenty(self):
+        self.assertEquals(self.wordclock.stringify(datetime.time(12, 19, 0)), "it is 12 20")
+
+    def testOneMinuteAfterTwenty(self):
+        self.assertEquals(self.wordclock.stringify(datetime.time(12, 21, 0)), "it is 12 20")
+
+    def testTwoMinutesAfterTwenty(self):
+        self.assertEquals(self.wordclock.stringify(datetime.time(12, 22, 0)), "it is 12 20")
+
+    def testThreeMinutesAfterTwenty(self):
+        self.assertEquals(self.wordclock.stringify(datetime.time(12, 23, 0)), "it is 12 25")
+
+
+
 if __name__ == '__main__':
     unittest.main()
